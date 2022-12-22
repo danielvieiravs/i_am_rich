2d3059e270ff0fadf2293b14815bc3777dd02705
Daniel Vieira <danielvieiravs@live.com.pt>
2022-12-22 11:03:36 +0000

    Complete APP

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World'
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'I Am Rich'
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image: AssetImage(
+              'images/diamond.png'
+            ),
+          ),
         ),
       ),
     ),
